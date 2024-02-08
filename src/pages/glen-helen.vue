a5a22c6cdc67d04ea9bf44b137465886284033fa
Mathew Mendoza <mathew.mendoza@appliedmedical.com>
2024-02-07 21:16:07 -0800

    Add glen helen page content

--- src/pages/glen-helen.vue
+++ src/pages/glen-helen.vue
@@ -1,18 +1,231 @@
 <template>
-  <main>
+  <main class="gh-content-container">
     <h1>
       Glen Helen RallyCross
     </h1>
     <p>
-      Glen Helen is the motocross park located near the city of Devore. Events are
-      ran on the south side parking lot of the facility. The terrain is very diverse
-      containing light dirt, heavy dirt, some gravel, some rocks and some sand. The
-      consistency of the dirt doesn’t change much once broken in and ruts are usually
-      minimal.
+      CalClub RallyX events at Glen Helen are sanctioned by the SCCA. These
+      events are held under the SCCA RallyX Rules and the following Supplemental
+      Regulations. Some Glen Helen RallyCross events count towards the California
+      Rally Series Rally Cross points. Please visit the Ridgecrest section for
+      more information.
     </p>
+    <section>
+      <h2>
+        Safety:
+      </h2>
+      <ul>
+        <li>
+          The Safety Steward of record has final say in all safety related issues.
+        </li>
+        <li>
+          All drivers and passengers must wear a Snell 2005 or better helmet.
+        </li>
+        <li>
+          Cars must pass a safety (tech) inspection before being allowed on course.
+          If the Event Chairman after consultation with the Event Safety Steward
+          determines at his discretion that a vehicle cannot safely negotiate the
+          course, it may be excluded.
+        </li>
+        <li>
+          Course is subject to immediate change if degradation is determined to cause a safety concern.
+        </li>
+        <li>
+          Speed limit is 5mph in the paddock.
+        </li>
+        <li>
+          Please be aware that this is a fairly isolated location with limited
+          emergency access. Some cell phone services have been determined to work
+          and the event management will have working communications. If a medical
+          or security issue should occur, whether related to the RallyX or not,
+          notify the Event Management to ascertain the problem.
+        </li>
+        <li>
+          Bring drinking water and food. Establishments are far enough away to
+          make a trip off the site inconvenient.
+        </li>
+        <li>
+          Photographers must stay within a normal spectator area unless given
+          specific permission by the Event Chair or Safety Steward. Permission
+          will at least require a spotter to accompany the photographer. All
+          photographers must check-in with registration/timing and give the
+          organizer contact information to receive photo(s) and/or video(s) for
+          event use and promotions.
+        </li>
+        <li>
+          Parking and driver paddoc is no longer allowed near the grid area (along the fence).
+        </li>
+        <li>
+          No spectating near the entry and exit points of the course area
+          (don’t stand near the gate).
+        </li>
+        <li>
+          Children must be accompanied with a parent, guardian, or care-taker at all times.
+          Children and dogs will not be allowed in the grid area. Dogs must leashed at all times.
+        </li>
+        <li>
+          Heavy machine operators must use equipment responsibly!
+        </li>
+      </ul>
+      <h3>
+        Passengers
+      </h3>
+      <p>
+        Unless otherwise noted passengers (navigators) will be allowed unrestricted.
+        Passengers must be at least 12 years old and have signed the SCCA waiver.
+        Competitors who have not complete all of their runs can not be a passenger.
+      </p>
+      <h3>
+        ATVs, Mountain Bikes, etc
+      </h3>
+      <p>
+        Personal vehicles of these types are not allowed in the grid area and can only
+        be operated on course with the permission of event management. These personal
+        vehicles are allowed in the paddock and outside of the event area, however extreme
+        caution should be used in their operation.
+      </p>
+    </section>
+    <section>
+      <h2>
+        Scoring and Prizes
+      </h2>
+      <ul>
+        <li>
+          We will be targeting 6 runs per entry.
+        </li>
+        <li>
+          Scored time will be the accumulation of all runs, including penalties.
+        </li>
+        <li>
+          A 2 second penalty will be given if a course pylon is upset or totally displaced
+          (including post finish cones).
+        </li>
+        <li>
+          A 10 second penalty will be given for each missed gate or off course.
+        </li>
+        <li>
+          Starting before receiving signal from the started will be considered an
+          off course penalty.
+        </li>
+        <li>
+          A 2 second penlty will be given to any pylon hit after the course finish
+          and/or unsafe transit back to grid.
+        </li>
+        <li>
+          warning will be given for unsafe transit back to grid, than a 10 second
+          penalty will be added for continued unsafe transit.
+        </li>
+        <li>
+          A 3rd time will disqualify the driver and no subsequent runs will be given.
+        </li>
+        <li>
+          For a mechanical DNF the driver will be given the slowest competitive time of
+          a driver’s normal group +10 seconds.
+        </li>
+        <li>
+          Accumulation of 10 cones over all runs will result in the driver being disqualified
+          from the event, i.e. the driver will not be allowed subsequent runs.
+        </li>
+        <li>
+          1st, 2nd, and 3rd place positions in each race class will be awarded a trophy custom.
+        </li>
+      </ul>
+      <p>
+        End of season trophies will be given to the top 3 drivers of each race class.
+        1st place champion in each race class will receive a custom CalClub RallyCross champion jacket.
+      </p>
+      <p>
+        In the event of a tie for season championship points standings, the total
+        cones hit for the entire season will be the tie breaker. The driver with the
+        least amount of cones hit for the entire season will take the lead. This tie
+        break situation will only pertain to the top 3 positions of each class.
+      </p>
+      <h2>
+        Position Based Points System
+      </h2>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">1st</th>
+            <th scope="col">2nd</th>
+            <th scope="col">3rd</th>
+            <th scope="col">4th</th>
+            <th scope="col">5th</th>
+            <th scope="col">6th</th>
+            <th scope="col">7th</th>
+            <th scope="col">8th</th>
+            <th scope="col">9th</th>
+            <th scope="col">10th</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>25pts</td>
+            <td>22pts</td>
+            <td>20pts</td>
+            <td>18pts</td>
+            <td>16pts</td>
+            <td>14pts</td>
+            <td>13pts</td>
+            <td>12pts</td>
+            <td>11pts</td>
+            <td>10pts</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+    <section>
+      <h2>
+        Classes
+      </h2>
+      <ul>
+        <li>S2 - SCCA Stock Front and Rear Wheel Drive</li>
+        <li>SA - SCCA Stock All Wheel Drive</li>
+        <li>P2 - SCCA Prepared Front and Rear Wheel Drive</li>
+        <li>PA - SCCA Prepared All Wheel Drive</li>
+        <li>M2 - SCCA Modified Front and Rear Wheel Drive</li>
+        <li>M4 - SCCA Modified All Wheel Drive</li>
+        <li>C2 - SCCA Constructors Two Wheel Drive</li>
+        <li>C4 - SCCA Constructors All Wheel Drive</li>
+      </ul>
+      <p>
+        CRS P-Stock and Group-2 log booked rally cars are now allowed to compete in P2
+      </p>
+      <h2>
+        Regional Allowances
+      </h2>
+      <p>
+        Radiator’s may be replaced with a similar sized core as oem in the stock classes.
+        Radiator cooling hoses may be replaced with silicon and must be routed the same as
+        oem. No other cooling modifications may be allowed unless otherwise stated in the
+        official SCCA rules. (It can get hot at Glen Helen)
+      </p>
+      <p>
+        Tire studs are not allowed in any CalClub RallyCross event!
+      </p>
+      <p>
+        Bumper’s may be replaced or removed. Stock class vehicles with a bash style bumper
+        will be classified as prepared. Vehicle’s with completely removed bumpers will be
+        classified as modified. National event’s require bumpers.
+      </p>
+      <h2>
+        Race Numbers
+      </h2>
+      <p>
+        All drivers are required to have proper numbers. Tape will still be allowed such
+        that the tape used must contrast on the vehicle. Number’s must be at least 8″ tall
+        and clearly visible on both sides of the vehicle.
+      </p>
+    </section>
   </main>
 </template>
 
 <script setup>
 
 </script>
+
+<style lang="scss" scoped>
+.gh-content-container {
+  padding: 48px $side-spacing;
+}
+</style>
